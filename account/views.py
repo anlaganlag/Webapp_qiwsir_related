492dc5e44339850f3964c689885392e08f2e373a
gal <tanjhu@163.com>
2019-12-12 15:08:38 +0800

    增加了注册项目

--- account/views.py
+++ account/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import authenticate,login
-from .forms import LoginForm,RegistrationForm
+from .forms import LoginForm,RegistrationForm,UserProfileForm
 
 
 def user_login(request):
@@ -22,18 +22,43 @@ def user_login(request):
         login_form = LoginForm()
         return render(request,'account/login.html',{'form':login_form})
 
+#def register(request):
+    #if request.method == 'POST':
+        #user_form = RegistrationForm(request.POST)
+        #userprofile_form=UserProfileForm(request.POST)
+#
+        #if user_form.is_valid()*userprofile_form.is_valid():
+            #new_user = user_form.save(commit=False)
+            #new_user.set_password(user_form.cleaned_data['password'])
+            #new_user.save()
+            #new_profile = userprofile_form.save(commit=False)
+            #new_profil.user = new_user
+            #new_profile.save()
+            #return HttpResponse('successfully')
+        #else:
+            #return HttpResponse('sorry,your can not register.')
+    #else:
+        #user_form = RegistrationForm()
+        #userprofile_form = UserProfileForm()
+        #return render(request,'account/register.html',{'form':user_form,"profile":userprofile_form})
+
+
+
 def register(request):
-    if request.method == 'POST':
-        user_form = RegistrationForm(request.POST)
-        if user_form.is_valid():
-            new_user = user_form.save(commit=False)
-            new_user.set_password(user_form.cleaned_data['password'])
+    if request.method == "POST":
+        user_form = RegistrationForm(request.POST) 
+        userprofile_form = UserProfileForm(request.POST)
+        if user_form.is_valid()*userprofile_form.is_valid():
+            new_user = user_form.save(commit=False) 
+            new_user.set_password(user_form.cleaned_data['password']) 
             new_user.save()
-            return HttpResponse('successfully')
+            new_profile = userprofile_form.save(commit=False) 
+            new_profile.user = new_user
+            new_profile.save()
+            return HttpResponse("successfully")
         else:
-            return HttpResponse('sorry,your can not register.')
+            return HttpResponse("sorry, your can not register.")
     else:
-        user_form = RegistrationForm()
-        return render(request,'account/register.html',{'form':user_form})
-
-
+        user_form = RegistrationForm() 
+        userprofile_form = UserProfileForm()
+        return render(request, "account/register.html", {"form": user_form, "profile":userprofile_form})
