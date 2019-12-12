86c01f04e4cbb34c1af3ee8a42f6ae70af623a67
gal <tanjhu@163.com>
2019-12-12 13:06:59 +0800

    完成用户注册相关代码

--- account/forms.py
+++ account/forms.py
@@ -6,12 +6,12 @@ class LoginForm(forms.Form):
     password = forms.CharField(widget=forms.PasswordInput)
 
 class RegistrationForm(forms.ModelForm):
-    password = forms.CharField(label="Password",widget=forms.PaaswordInput)
+    password = forms.CharField(label="Password",widget=forms.PasswordInput)
     password2= forms.CharField(label='Confirm Password',widget=forms.PasswordInput)
 
     class Meta:
         model =User
-        field = {'username','email')
+        fields = {'username','email'}
     def clean_password2(self):
         cd = self.cleaned_data
         if cd['password'] != cd['password2']:
